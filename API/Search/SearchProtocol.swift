6ee54583173b3d9d07752bfe501caead7621b11a
48rkan <erkanemiraslan@gmail.com>
2023-05-23 02:11:37 +0400

    Update SearchProtocol.swift

--- API/Search/SearchProtocol.swift
+++ API/Search/SearchProtocol.swift
@@ -6,7 +6,8 @@
 import Foundation
 
 protocol SearchProtocol {
-    static func fetchMatchesGifs(text: String,completion: @escaping (Trending?,Error?)->())
-    
-    static func fetchRelativeChannel(query: String,completion: @escaping (RelativeChannel?,Error?)->())
+    static func fetchMatchesGifs(text: String,
+                                 completion: @escaping (Trending?,Error?)->())
+    static func fetchRelativeChannel(query: String,
+                                     completion: @escaping (RelativeChannel?,Error?)->())
 }
